2a0c70dbc77de4daafac06ad9f68b66ef3b6338e
Ivy Murphy <ivy@ivys-mbp.myfiosgateway.com>
2024-02-27 19:58:56 -0500

    Lil fixes

---
+++ build/precache-manifest.52ebb535c094f70c78d0c097f8805e95.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f261b1744fc4a4e757961f4d42f26181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a69477a8a5cf6f2fa018",
+    "url": "/static/css/main.9f09b68a.chunk.css"
+  },
+  {
+    "revision": "af2737ddceb3207e8f0d",
+    "url": "/static/js/2.93862d78.chunk.js"
+  },
+  {
+    "revision": "13112e7efb20a443c75b02825974722e",
+    "url": "/static/js/2.93862d78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69477a8a5cf6f2fa018",
+    "url": "/static/js/main.4eb9ba18.chunk.js"
+  },
+  {
+    "revision": "4aa91a3cc4ddc7b62f5b",
+    "url": "/static/js/runtime-main.327ee50f.js"
+  },
+  {
+    "revision": "aa4cc8e788b8335032ac3c518719ea73",
+    "url": "/static/media/Ivy_Resume_2024.aa4cc8e7.PDF"
+  },
+  {
+    "revision": "7e4bd7598efa1ca1e48c1d6682ce8488",
+    "url": "/static/media/Lockal-Regular.7e4bd759.otf"
+  },
+  {
+    "revision": "070cf336ff89e150b11910acdd811fb1",
+    "url": "/static/media/Lockal-RoughOblique.070cf336.otf"
+  },
+  {
+    "revision": "f8127b6e91f1ab6d8fa59919ddfb11c7",
+    "url": "/static/media/ProximaNova-Bold.f8127b6e.otf"
+  },
+  {
+    "revision": "d613ab258b13358e3997961032844a48",
+    "url": "/static/media/ProximaNova-Regular.d613ab25.otf"
+  },
+  {
+    "revision": "6e0bf520d992e875f35eebc8c282ebf6",
+    "url": "/static/media/ivy_portrait.6e0bf520.png"
+  }
+]);
